f64c696e33069b07b3ff87357b6a9f1042bb6a18
Aditya Rout <adi.b20612@gmail.com>
2022-09-26 09:39:34 +0530

    feat: delegate should vote for all its deligating addresses at once

--- src/dill/gauge-proxies/gauge-proxy-v2.sol
+++ src/dill/gauge-proxies/gauge-proxy-v2.sol
@@ -219,7 +219,7 @@ contract GaugeProxyV2 is ProtocolGovernance, Initializable {
         // Period => Boolean (if delegate address can vote in that period)
         mapping(uint256 => bool) blockDelegate;
     }
-
+    mapping(address => address[]) public delegatedAddresses;
     mapping(address => delegateData) public delegations;
 
     function getCurrentPeriodId() public view returns (uint256) {
@@ -411,6 +411,10 @@ contract GaugeProxyV2 is ProtocolGovernance, Initializable {
 
         uint256 currentPeriodId = getCurrentPeriodId();
 
+        if (_delegate.delegate != _delegateAddress) {
+            delegatedAddresses[_delegateAddress].push(msg.sender);
+        }
+
         address currentDelegate = _delegate.delegate;
         _delegate.delegate = _delegateAddress;
         _delegate.prevDelegate = currentDelegate;
@@ -426,7 +430,6 @@ contract GaugeProxyV2 is ProtocolGovernance, Initializable {
     }
 
     function voteFor(
-        address _owner,
         address[] calldata _tokenVote,
         int256[] calldata _weights
     ) external {
@@ -437,26 +440,29 @@ contract GaugeProxyV2 is ProtocolGovernance, Initializable {
 
         uint256 currentId = getCurrentPeriodId();
         require(currentId > 0, "Voting not started yet");
-        delegateData storage _delegate = delegations[_owner];
-        require(
-            (_delegate.delegate == msg.sender &&
-                currentId > _delegate.updatePeriodId) ||
-                (_delegate.prevDelegate == msg.sender &&
-                    currentId == _delegate.updatePeriodId) ||
-                (_delegate.prevDelegate == address(0) &&
-                    currentId == _delegate.updatePeriodId),
-            "Sender not authorized"
-        );
-        require(
-            _delegate.blockDelegate[currentId] == false,
-            "Delegating address has already voted"
-        );
-        require(
-            (_delegate.indefinite || currentId <= _delegate.endPeriod),
-            "Delegating period expired"
-        );
 
-        _vote(_owner, _tokenVote, _weights, currentId);
+        address[] _deletgatedAddress = delegatedAddresses[msg.sender];
+
+        require(_deletgatedAddress.length > 0, "No delegating address found");
+
+        for (uint256 i = 0; i < _deletgatedAddress.length; i++) {
+            address _owner = _deletgatedAddress[i];
+            delegateData storage _delegate = delegations[_owner];
+
+            if (
+                ((_delegate.delegate == msg.sender &&
+                    currentId > _delegate.updatePeriodId) ||
+                    (_delegate.prevDelegate == msg.sender &&
+                        currentId == _delegate.updatePeriodId) ||
+                    ((_delegate.prevDelegate == address(0) &&
+                        currentId == _delegate.updatePeriodId) &&
+                        (_delegate.indefinite ||
+                            currentId <= _delegate.endPeriod))) &&
+                (_delegate.blockDelegate[currentId] == false)
+            ) {
+                _vote(_owner, _tokenVote, _weights, currentId);
+            }
+        }
     }
 
     // Add new token gauge
@@ -665,7 +671,7 @@ contract GaugeProxyV2 is ProtocolGovernance, Initializable {
                     PICKLE.safeApprove(_gauge.gaugeAddress, 0);
                     PICKLE.safeApprove(_gauge.gaugeAddress, reward_);
                     iGaugeV2(_gauge.gaugeAddress).notifyRewardAmount(
-                        address(TempPICKLE),
+                        address(PICKLE),
                         reward_,
                         _weights,
                         periodToUse
