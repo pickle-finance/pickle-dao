1c328dd3ab83cdd6c744904fb2192272af96d4b1
Aditya Rout <adi.b20612@gmail.com>
2022-10-06 15:27:06 +0530

    fix: add comments

--- src/dill/gauge-proxies/sidechain-gauge-proxy.sol
+++ src/dill/gauge-proxies/sidechain-gauge-proxy.sol
@@ -2,13 +2,11 @@
 pragma solidity ^0.8.1;
 
 import "../ProtocolGovernance.sol";
-// import "@openzeppelin/contracts/interfaces/IERC20Upgradeable.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
-interface iGaugeV2 {
+interface IGaugeV2 {
     function notifyRewardAmount(
         address rewardToken,
         uint256 rewards,
@@ -20,11 +18,11 @@ interface iGaugeV2 {
 contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    IERC20Upgradeable public PICKLE;
-
+    uint256 public periodToDistribute;
     address[] internal _tokens;
     address bridgeClient;
 
+    /* ========== ENUM & STRUCTS ========== */
     enum GaugeType {
         REGULAR,
         VIRTUAL
@@ -35,18 +33,21 @@ contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
         address gaugeAddress;
     }
 
-    mapping(address => Gauge) public gauges;
-    mapping(uint256 => uint256) public periodRewardAmount;
-    mapping(uint256 => int256[]) public periodGaugeWeights;
-
     struct periodData {
         uint256 amount;
         int256[] weights;
     }
+
+    /* ========== MAPPINGS ========== */
+    mapping(address => Gauge) public gauges;
+    mapping(uint256 => uint256) public periodRewardAmount;
+    mapping(uint256 => int256[]) public periodGaugeWeights;
     mapping(uint256 => periodData) public periods; // periodID => periodData
     mapping(uint256 => bool) public distributedForPeriod;
-    uint256 public periodToDistribute;
 
+    IERC20Upgradeable public PICKLE;
+
+    /* ========== VIEWS ========== */
     function tokens() external view returns (address[] memory) {
         return _tokens;
     }
@@ -55,18 +56,29 @@ contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
         return gauges[_token];
     }
 
+    /* ========== INITIALIZER ========== */
+    /**
+     * @notice  Initializer
+     * @param   _pickleToken  Address of pickle token on sidechain
+     * @param   _bridgeClient  Address of Anycall bridge client
+     * @param   _periodToDistribute  Initial period to start distribution from
+     */
     function initialize(
-        address pickleToken,
+        address _pickleToken,
         address _bridgeClient,
         uint256 _periodToDistribute
     ) public initializer {
         periodToDistribute = _periodToDistribute;
         governance = msg.sender;
-        PICKLE = IERC20Upgradeable(pickleToken);
+        PICKLE = IERC20Upgradeable(_pickleToken);
         bridgeClient = _bridgeClient;
     }
 
-    // Add new token gauge
+      /**
+     * @notice  Add new gauge
+     * @param   _token  Address of gauge token
+     * @param   _gaugeAddress  Address of gauge
+     */
     function addGauge(address _token, address _gaugeAddress) external {
         require(msg.sender == governance, "!gov");
         require(gauges[_token].gaugeAddress == address(0x0), "exists");
@@ -77,10 +89,13 @@ contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
         _tokens.push(_token);
     }
 
-    // Add new token virtual gauge
+       /**
+     * @notice  Add new virtual gauge
+     * @param   _token  Address of gauge token
+     * @param   _gaugeAddress  Address of gauge
+     */
     function addVirtualGauge(
         address _token,
-        address _jar,
         address _gaugeAddress
     ) external {
         require(msg.sender == governance, "!gov");
@@ -93,6 +108,12 @@ contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
         _tokens.push(_token);
     }
 
+    /**
+     * @notice  Distribute rewards to gauges
+     * @dev     _start and _end are taken to avoid out of gas error in case large number of gauges are listed
+     * @param   _start  Starting index of gauges
+     * @param   _end  Ending index of gauges
+     */
     function distribute(uint256 _start, uint256 _end) external {
         require(_start < _end, "SidechainGaugeProxy: bad _start");
         require(_end <= _tokens.length, "SidechainGaugeProxy: bad _end");
@@ -134,7 +155,7 @@ contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
                     uint256 reward_ = uint256(_reward);
                     PICKLE.safeApprove(_gaugeAddress, 0);
                     PICKLE.safeApprove(_gaugeAddress, reward_);
-                    iGaugeV2(_gaugeAddress).notifyRewardAmount(
+                    IGaugeV2(_gaugeAddress).notifyRewardAmount(
                         address(PICKLE),
                         reward_,
                         new int256[](0),
@@ -149,6 +170,12 @@ contract SidechainGaugeProxy is ProtocolGovernance, Initializable {
         }
     }
 
+    /**
+     * @notice  Fetch rewards data 
+     * @param   _periodId  PeriodId for which rewards data is fetched
+     * @param   _amount  Amount to be distributed for _periodId's distribution
+     * @param   _weights  Gauge weights for _periodId 
+     */
     function sendRewards(
         uint256 _periodId,
         uint256 _amount,
